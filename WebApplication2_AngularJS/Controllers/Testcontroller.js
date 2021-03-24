d7ab98126cee1c4ebc76142da72b1d02ccafe18b
Aquafist <54714499+Aquafist@users.noreply.github.com>
2021-03-24 19:12:36 +0530

    First commit

---
+++ WebApplication2_AngularJS/Controllers/Testcontroller.js
@@ -0,0 +1,19 @@
+﻿app.controller('myController', function ($scope) {
+    $scope.products = ["The Monk Who Sold His Ferrar", "Death in the Cloud", " Keep Off the Grass"];
+    $scope.addItems = function () {
+        $scope.errortext = "";
+        if (!$scope.addInList) {
+            return;
+        }
+        if ($scope.products.indexOf($scope.addInList) == -1) {
+            $scope.products.push($scope.addInList);
+        }
+        else {
+            $scope.errortext = "The item is already in your shopping list.";
+        }
+    }
+    $scope.removeItem = function (x) {
+        $scope.errortext = "";
+        $scope.products.splice(x, 1);
+    }
+});
